47a4db185a32fb41017f0e95380559c4c799be67
Bruno Meilick <b@bnomei.com>
2018-02-02 13:02:00 +0100

    support for rss feed that are not page objects but just urls
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- snippets/htmlhead-feed.php
+++ snippets/htmlhead-feed.php
@@ -7,6 +7,12 @@
 				->attr('type', 'application/rss+xml')
 				->attr('href',  $htmlhead_feedpage->url())
 				->attr('title', $htmlhead_feedpage->title()).PHP_EOL;
+		} else {
+			echo brick('link')
+				->attr('rel',  'alternate')
+				->attr('type', 'application/rss+xml')
+				->attr('href',  url($htmlhead_feed))
+				->attr('title', $htmlhead_feed).PHP_EOL;
 		}
 	}
 ?>
